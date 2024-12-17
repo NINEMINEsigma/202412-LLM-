604679d9bfd0f4b3d31f22fa00d39f00a202b5d3
ninemine <106434473+NINEMINEsigma@users.noreply.github.com>
2024-12-18 06:20:00 +0800

    Save

---
+++ project/automated_test.py
@@ -0,0 +1,21 @@
+from project.selunit            import *
+from lekit.File.Core            import tool_file
+from lekit.LLM.LangChain.llama  import light_llama_core
+from project.Core               import ProjectConfig
+
+SampleTest_LLm_Model_Path_FindKey   = r"SampleTest_LLM_Model"
+SampleTest_LLM_Model_Max_Ctx_Len    = r"SampleTest_LLM_n_ctx"
+
+class AutomatedTestCore:
+    def __init__(self, start_url:str):
+        self.begin_url = start_url
+
+    def run(auto_runtime_path_config_file:tool_file):
+        current_config: ProjectConfig   = ProjectConfig()
+        current_unit:   selunit         = selunit()
+    
+    
+    
+    
+    
+    
--- project/sample_test.py
+++ project/sample_test.py
@@ -4,15 +4,8 @@ from typing                     import *
 from lekit.File.Core            import tool_file
 from lekit.Str.Core             import UnWrapper
 from docx.document              import Document as DocumentObject
-from project.Core                       import ProjectConfig
+from project.Core               import ProjectConfig
 from lekit.LLM.LangChain.llama  import *
-
-defined_names = {
-    "Scene":    "Scene", 
-    "Module":   "Module",
-    "Operator": "Operator",
-    "Expect":   "Expect"
-}
 
 class DocxRuntime:
     def __init__(self, file:Union[str, tool_file]):
@@ -44,8 +37,9 @@ SampleTest_KeysTemplate = {
     "Expect":"expected result"
 }
 
-SampleTest_LLm_Model_Path_FindKey = r"SampleTest_LLM_Model"
-SampleTest_LLm_Model_SystemPrompt = '''
+SampleTest_LLm_Model_Path_FindKey   = r"SampleTest_LLM_Model"
+SampleTest_LLM_Model_Max_Ctx_Len    = r"SampleTest_LLM_n_ctx"
+SampleTest_LLm_Model_SystemPrompt   = '''
 You are a software testing engineer who is completing a software testing task.
 You will then receive a design document for the software, and you will need to get all the test cases you should have based on this document.
 You need to output a list of test cases in a specific format.
@@ -58,7 +52,7 @@ The format is as follows.
     ]
 }
 
-In particular, please translate the output into the language corresponding to the document.
+In particular, please translate the output into Chinese
 '''
 
 class SampleTestCore:
@@ -67,19 +61,35 @@ class SampleTestCore:
         self.web_codes:     Dict[WebCodeType, Dict[URL_or_Marking_Type,str]]    = {}
         
     def run(self):
+        # Check config and get target property
         current_config:ProjectConfig = ProjectConfig()
         stats = True
+        target_n_ctx = 512
         if SampleTest_LLm_Model_Path_FindKey not in current_config or current_config[SampleTest_LLm_Model_Path_FindKey] == "set you model":
             current_config.LogPropertyNotFound(SampleTest_LLm_Model_Path_FindKey)
             current_config[SampleTest_LLm_Model_Path_FindKey] = "set you model"
             stats = False
+        if SampleTest_LLM_Model_Max_Ctx_Len not in current_config:
+            current_config.LogWarning(f"{SampleTest_LLM_Model_Max_Ctx_Len} is not found, current using default[{target_n_ctx}]")
             
         if stats is False:
             current_config.save_properties()
             return
             
-        llm_core:light_llama_core       = light_llama_core(tool_file(current_config[SampleTest_LLm_Model_Path_FindKey]),
-            make_system_prompt(SampleTest_LLm_Model_SystemPrompt))
+        # Build llm core
+        llm_core:light_llama_core       = light_llama_core(ChatLlamaCpp(
+            model_path=UnWrapper(tool_file(current_config[SampleTest_LLm_Model_Path_FindKey])), n_ctx=target_n_ctx
+            ),
+            init_message=make_system_prompt(SampleTest_LLm_Model_SystemPrompt))
+        
+        # Build up auto runtime path
+        assets_target_file = current_config.get_file("StreamingAssets/AutoRuntimePath.json", True)
+        list_text = []
         for text in self.docx_runtime.read_all():
-            print(llm_core(text))
+            list_text.append(llm_core(text).content)
+        assets_target_file.open('w')
+        assets_target_file.data={
+            "Datas":list_text
+        }
+        assets_target_file.save()
         
